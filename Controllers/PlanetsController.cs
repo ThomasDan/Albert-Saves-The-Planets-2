7afe9606982596d03a267a7998205501d63ca454
Thomas Carl Alexander Dänhardt <thomas.danhardt@gmail.com>
2022-03-03 13:38:02 +0100

    added planets

---
+++ Controllers/PlanetsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Albert_Saves_The_Planets_2.Controllers
+{
+    public class PlanetsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
