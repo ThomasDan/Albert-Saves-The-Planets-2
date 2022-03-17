c2f834a69d0eba2ffa802ec3679fe9999c62e174
Thomas Carl Alexander Dänhardt <thomas.danhardt@gmail.com>
2022-03-17 09:40:08 +0100

    pepee pa poooopoooo

---
+++ DAL/WebSocketManager.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Albert_Saves_The_Planets_2.DAL
+{
+    internal class WebSocketManager
+    {
+
+    }
+}
