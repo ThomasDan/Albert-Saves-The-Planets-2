a32e2a45a24638b5d23237b47e6b254092e4b4a0
Thomas Carl Alexander Dänhardt <thomas.danhardt@gmail.com>
2022-03-17 20:43:26 +0100

    SPARE TIME IS AN ILLUSION SLEEP IS FOR THE WEAK ARBEIT MACHT FREI 01010000 01010010 01000001 01001001 01010011 01000101 00100000 01010100 01001000 01000101 00100000 01001111 01001101 01001110 01001001 01010011 01010011 01001001 01000001 01001000 00100001 00100000 01001101 01000001 01011001 00100000 01001000 01000101 00100000 01000010 01001100 01000101 01010011 01010011 00100000 01010101 01010011 00100000 01010111 01001001 01010100 01001000 00100000 01010100 01001000 01000101 00100000 01000011 01000101 01010010 01010100 01000001 01001001 01001110 01010100 01011001 00100000 01001111 01000110 00100000 01010011 01010100 01000101 01000101 01001100 00100001 00100000 01010000 01001111 01010111 01000101 01010010 00100000 01010100 01001111 00100000 01001000 01001111 01001100 01011001 00100000 01001101 01000001 01010010 01010011 00100001

--- wwwroot/js/solarsystem.js
+++ wwwroot/js/solarsystem.js
@@ -1,14 +1,18 @@
-$(document).ready(function(e) {
-            $('img[usemap]').rwdImageMaps();
+// Messed with the e, ee, and eee below
+// (They were all named "e" before..so not sure if this break stuff : DDDD )
+//  - Thomas 
+$(document).ready(function (e) {
+    $('img[usemap]').rwdImageMaps();
 
-            $('area').on('focus', function(e) {
-                e.preventDefault();
-                $('.selection p').html($(this).attr('class'));
-            });
+    $('area').on('focus', function (ee) {
+        ee.preventDefault();
+        $('.selection p').html($(this).attr('class'));
+    });
 
-            $(document).on('click', function(e) {
-                e.preventDefault();
-                if ($(e.target).closest('area').length === 0) {
-                    $('.selection p').html('click a brick');
-                }
-            });
+    $(document).on('click', function (eee) {
+        eee.preventDefault();
+        if ($(eee.target).closest('area').length === 0) {
+            $('.selection p').html('click a brick');
+        }
+    });
+});
